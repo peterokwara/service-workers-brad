d68250ca46b11753ce027dbf8e7ef1919e3ee559
peter.o <peter.okwara@outlook.com>
2020-01-20 19:32:25 +0300

    Cache assets during the install event

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -1,6 +1,22 @@
+const cacheName = 'v1';
+
+const cacheAssets = [
+    'index.html',
+    'about.html',
+    '/css/style.css',
+    '/js/main.js'
+];
+
 // call install event
 self.addEventListener('install', (event) => {
     console.log('Service worker installed! 😀')
+
+    event.waitUntil(
+        caches.open(cacheName).then(cache => {
+            console.log('Service Worker: Caching Files 🧐')
+            cache.addAll(cacheAssets)
+        }).then(() => self.skipWaiting())
+    );
 })
 
 // call activate event
