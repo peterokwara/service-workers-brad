4b1164128cc64ce64fbb997754b432259d53486b
peter.o <peter.okwara@outlook.com>
2020-01-20 19:01:31 +0300

    Console log the activation event of the service worker

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -2,3 +2,8 @@
 self.addEventListener('install', (event) => {
     console.log('Service worker installed! 😀')
 })
+
+// call activate event
+self.addEventListener('activate', (event) => {
+    console.log('Service worker activated! 😀')
+})
