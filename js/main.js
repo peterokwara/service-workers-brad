b4b60810b671ead134ba5176246dbde98b6077a6
peter.o <peter.okwara@outlook.com>
2020-01-20 18:53:27 +0300

    Register the service worker

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 // make sure service workers are supported
-if ("serviceWorker" in navigator) {
-  console.log("Service worker supported 😃")
-}
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('../sw_cached_pages.js')
+      .then(reg => console.log('Service Worker: Registered 😀'))
+      .catch(err => console.log(`Service Worker: Error: ${err} 😐`))
+  })
+
+} 
